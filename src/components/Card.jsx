1fe54d58a8245d74307d20ba8b8bc8cbac9a6f34
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2024-12-30 20:54:49 +0530

    h1 classname done

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 function Card() {
   return (
     <div className='w-52 h-full bg-zinc-100 rounded-lg flex flex-col items-center p-2'>
-      <div className='image w-[3vw] h-[3vw] rounded-full bg-blue-800 overflow-hidden'>
+      <div className='image w-[3vw] h-[3vw] rounded-full bg-zinc-200 overflow-hidden'>
         <img className='w-full h-full object-cover' src="" alt="" />
       </div>
-      <h1>Vishwajeet</h1>
+      <h1 className='text-xl font-semibold'>Vishwajeet</h1>
       <p className='text-center text-xs font-semibold leading-1 tracking-tight'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis, enim.</p>
     </div>
   )
