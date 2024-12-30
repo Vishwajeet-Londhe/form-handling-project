3d31543c697c1da8a0faa29e9554684a8f42b89b
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2024-12-30 20:47:36 +0530

    name and p

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 function Card() {
   return (
-    <div className='w-32 h-full bg-sky-300 rounded-lg flex flex-col items-center p-2'>
-      <div className='image w-10 h-10 rounded-full bg-blue-800'>
+    <div className='w-52 h-full bg-sky-300 rounded-lg flex flex-col items-center p-2'>
+      <div className='image w-[3vw] h-[3vw] rounded-full bg-blue-800 overflow-hidden'>
         <img className='w-full h-full object-cover' src="" alt="" />
       </div>
+      <h1>Vishwajeet</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis, enim.</p>
     </div>
   )
 }
