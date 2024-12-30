c616a8fcbe054782cc7a649aa5d34288ee8b74fb
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2024-12-30 20:44:01 +0530

    card image

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 function Card() {
   return (
-    <div className='w-32 h-full bg-sky-300'>
-      
+    <div className='w-32 h-full bg-sky-300 rounded-lg flex flex-col items-center p-2'>
+      <div className='image w-10 h-10 rounded-full bg-blue-800'>
+        <img className='w-full h-full object-cover' src="" alt="" />
+      </div>
     </div>
   )
 }
