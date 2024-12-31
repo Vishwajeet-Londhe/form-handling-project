163ba3c0dabe757b5678c11662cf2ee2257762e3
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2024-12-31 13:13:14 +0530

    placeHolder

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,9 +4,9 @@ function form() {
   return (
     <div className='mt-10 flex justify-center'>
       <form className='flex gap-10' action="">
-        <input className='rounded-md px-2 py-1 text-base font-semibold outline-none' type="text" />
-        <input className='rounded-md px-2 py-1 text-base font-semibold outline-none' type="text" />
-        <input className='rounded-md px-2 py-1 text-base font-semibold outline-none' type="text" />
+        <input className='rounded-md px-2 py-1 text-base font-semibold outline-none' type="text" placeholder='name' />
+        <input className='rounded-md px-2 py-1 text-base font-semibold outline-none' type="text" placeholder='email' />
+        <input className='rounded-md px-2 py-1 text-base font-semibold outline-none' type="text" placeholder='image url' />
       </form>
     </div>
   )
