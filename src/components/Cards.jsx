94dcab5d0e541e74e20181c4b7fe3729f7fd6df6
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2024-12-31 12:51:53 +0530

    max h and overflow auto added

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -3,7 +3,7 @@ import Card from './Card'
 
 function Cards() {
   return (
-    <div className='w-ful p-4 flex justify-center gap-4 flex-wrap'>
+    <div className='w-ful p-4 flex justify-center gap-4 flex-wrap max-h-96 overflow-auto'>
       <Card />
       <Card />
       <Card />
