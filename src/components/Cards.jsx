dd186cb53e993e4a53752cd7d00fe5dce3d8f6be
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2024-12-31 12:50:40 +0530

    cards className

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -3,7 +3,19 @@ import Card from './Card'
 
 function Cards() {
   return (
-    <div className='w-ful p-4'>
+    <div className='w-ful p-4 flex justify-center gap-4 flex-wrap'>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
       <Card />
     </div>
   )
