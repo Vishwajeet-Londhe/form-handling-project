cc6d900d6c5db3f53be68b2dd4307347aa2d0608
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2024-12-31 13:08:20 +0530

    form input with classname

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 function form() {
   return (
     <div className='mt-10 flex justify-center'>
-      <h1>jej</h1>
+      <form className='flex gap-10' action="">
+        <input className='rounded-md px-2 py-1 text-base font-semibold outline-none' type="text" />
+        <input className='rounded-md px-2 py-1 text-base font-semibold outline-none' type="text" />
+        <input className='rounded-md px-2 py-1 text-base font-semibold outline-none' type="text" />
+      </form>
     </div>
   )
 }
