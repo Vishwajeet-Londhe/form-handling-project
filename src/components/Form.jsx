9d6a65d09c48e2f65743db538f6b83f862205e41
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2024-12-31 13:14:14 +0530

    submit button with classname

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,7 +6,8 @@ function form() {
       <form className='flex gap-10' action="">
         <input className='rounded-md px-2 py-1 text-base font-semibold outline-none' type="text" placeholder='name' />
         <input className='rounded-md px-2 py-1 text-base font-semibold outline-none' type="text" placeholder='email' />
-        <input className='rounded-md px-2 py-1 text-base font-semibold outline-none' type="text" placeholder='image url' />
+        <input className='rounded-md px-2 py-1 text-base font-semibold outline-none' type="text" placeholder='image url'/>
+        <input type="submit" />
       </form>
     </div>
   )
