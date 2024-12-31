4ad436d8b7fcd8fd8eac5a3370eca6cf26de2c75
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2024-12-31 12:52:30 +0530

    working on single card

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -5,18 +5,6 @@ function Cards() {
   return (
     <div className='w-ful p-4 flex justify-center gap-4 flex-wrap max-h-96 overflow-auto'>
       <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
     </div>
   )
 }
