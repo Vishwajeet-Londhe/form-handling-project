16bdb0db2f35fc14cb3b9d3928d489f74240f513
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-01-04 22:13:59 +0530

    register added to each section

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
 
 function form() {
+  const {register, handleSubmit} = useForm()
   return (
     <div className='mt-10 flex justify-center'>
       <form className='flex gap-10' action="">
-        <input className='rounded-md px-2 py-1 text-base font-semibold outline-none' type="text" placeholder='name' />
-        <input className='rounded-md px-2 py-1 text-base font-semibold outline-none' type="text" placeholder='email' />
-        <input className='rounded-md px-2 py-1 text-base font-semibold outline-none' type="text" placeholder='image url'/>
+        <input {...register('name')} className='rounded-md px-2 py-1 text-base font-semibold outline-none' type="text" placeholder='name' />
+        <input {...register('email')} className='rounded-md px-2 py-1 text-base font-semibold outline-none' type="text" placeholder='email' />
+        <input {...register('image')} className='rounded-md px-2 py-1 text-base font-semibold outline-none' type="text" placeholder='image url'/>
         <input className='rounded-md px-5 py-1 bg-blue-500 text-white font-semibold' type="submit" />
       </form>
     </div>
