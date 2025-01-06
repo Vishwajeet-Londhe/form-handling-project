54e768890d87817547130c50f0a32ab62e30981a
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-01-06 21:08:59 +0530

    function created to receive child data

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ import Form from './components/Form'
 
 function App() {
   const [users, setUsers] =  useState([]);
+
+  const handleFormSubmitData =(data)=>{
+    setUsers([...users, data])
+  }
+
   return (
     <div className='w-full h-screen bg-zinc-200 flex items-center justify-center'>
       <div className='container mx-auto'>
