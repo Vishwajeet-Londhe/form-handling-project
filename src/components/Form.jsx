158d5840b08411cf852b92aea015cba072143e16
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-01-07 16:30:36 +0530

    reset form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,10 +3,15 @@ import React from 'react'
 import { useForm } from 'react-hook-form'
 
 function form({handleFormSubmitData}) {
-  const {register, handleSubmit} = useForm()
+  const {register, handleSubmit, reset} = useForm()
+
+  const handleFormSubmit = (data) => {
+    handleFormSubmitData(data);
+    reset();
+  } 
   return (
     <div className='mt-10 flex justify-center'>
-      <form className='flex gap-10' onSubmit={handleSubmit(data=> handleFormSubmitData(data))}>
+      <form className='flex gap-10' onSubmit={handleSubmit(handleFormSubmit)}>
         <input {...register('name')} className='rounded-md px-2 py-1 text-base font-semibold outline-none' type="text" placeholder='name' />
         <input {...register('email')} className='rounded-md px-2 py-1 text-base font-semibold outline-none' type="text" placeholder='email' />
         <input {...register('image')} className='rounded-md px-2 py-1 text-base font-semibold outline-none' type="text" placeholder='image url'/>
